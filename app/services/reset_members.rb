72d688f599ceae8e39f6e58218a4a6fef4ca450d
Bruno Oliveira <bruno@abstractj.org>
2012-06-18 11:21:48 -0300

    Updated torquebox configuration files to support services

--- app/services/reset_members.rb
+++ app/services/reset_members.rb
@@ -1,10 +1,16 @@
 class ResetMembers
 
     def initialize(args = {})
-        Members.delete_all
+        @description = args['description']
     end
 
-    def start
+    def start 
+        Member.delete_all
         Member.create(:email => 'john.smith@mailinator.com', :name => 'John Smith', :phone_number => '2125551212')
+        puts "#{@description} was initialized"
+    end 
+
+    def stop 
+        puts "#{@description} was interrupted"
     end
 end
